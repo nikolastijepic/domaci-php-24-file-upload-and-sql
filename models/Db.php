48227671100902858cb089a7c6cd3c50848561db
nikolastijepic <nikola.stijepic94@icloud.com>
2026-05-17 20:26:55 +0200

    Create Db.php

---
+++ models/Db.php
@@ -0,0 +1,9 @@
+<?php
+
+class Db {
+    public $connection;
+
+    public function __construct() {
+        $this->connection = mysqli_connect("localhost", "root", "ServBay.dev", "php23");
+    }
+}
